e4f27441ec018b4f3b61b39e772020e38b723188
Raquel_Ortiz <raquel_ortiz@epam.com>
2024-04-05 16:01:18 +0200

    router works

---
+++ angular_project/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import {AppRoutingModule} from './app-routing.module';
+@NgModule({
+  imports: [
+  RoutingModule,
+  AppRoutingModule],
+})
+export class AppRoutingModule { }
